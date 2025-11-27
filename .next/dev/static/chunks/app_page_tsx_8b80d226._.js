6025ea05dd6780c17a3a812424d33002da130e0c
SEIDY-KANTE <seidykante21@gmail.com>
2025-11-27 21:04:33 +0300

    feat: Add new pages for credits, payment, and manual studio, along with a pricing component and a new store.

--- .next/dev/static/chunks/app_page_tsx_8b80d226._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_080854a9._.js",
-  "static/chunks/_65995427._.js"
-],
-    source: "dynamic"
-});
